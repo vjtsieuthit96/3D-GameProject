7650fbebf42ff824748d504609c3fe41a19c1b72
vjtsieuthit96 <vonguyenhung96@gmail.com>
2025-03-01 17:52:25 +0700

    adjust dragon skill

--- Assets/Script/Enemy/Monster/MountainDragon/MD_SkillManager.cs
+++ Assets/Script/Enemy/Monster/MountainDragon/MD_SkillManager.cs
@@ -170,7 +170,7 @@ public class MD_SkillManager : MonoBehaviour
     {
         for (int i=0;i<duration;i++)
         {
-            yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(1f);
             Vector3 spawnLocation = GetRandomPositionAroundTarget();
             Instantiate(_meteorRain, spawnLocation, Quaternion.identity);            
         }
